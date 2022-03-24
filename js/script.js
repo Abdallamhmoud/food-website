cadcc0bbb8c3803c6925f131a80eee37c4b0dd17
abdalla mahmoud <abdallamahmoud263@gmail.com>
2022-03-24 22:36:29 +0200

    restuarant

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 90){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+// let myInputs = document.querySelector(".order .row form .inputBox input");
+
+// myInputs.addEventListener("click",(e) =>{
+//     e.style.background = red;
+// });
